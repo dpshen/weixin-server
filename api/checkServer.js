b692913ae1d3071ea639afd1dd8fe336d4401494
shendapeng <shendapeng@yuantutech.com>
2016-10-12 17:06:48 +0800

    “”

--- api/checkServer.js
+++ api/checkServer.js
@@ -1,5 +1,6 @@
 import {requestAccessToken} from '../server/security'
 var uuid = require('uuid');
+var sha1 = require('sha1');
 
 export async function checkServer(ctx, next) {
     let query = ctx.request.query;
@@ -13,11 +14,25 @@ export async function checkServer(ctx, next) {
 export async function getJsApiTicket(ctx, next) {
     var wx = global.weixin;
     ctx.logger.trace("request jsApiTicket: ", wx.js_ticket);
+    ctx.logger.info(ctx.request.headers.referer);
+    var url = ctx.request.headers.referer || "";
+    if (url.includes('#')){
+        url = url.split('#')[0]
+    }
+
+    var ret = {
+        url:url,
+        appid:wx.appid,
+        ticket: wx.js_ticket,
+        nonceStr: uuid.v1(),
+        timestamp: new Date().getTime()
+    }
+    var unStr = `jsapi_ticket=${ret.ticke}&noncestr=${ret.nonceStr}&timestamp=${ret.timestamp}&url=${url}`;
+
+    ret.signature = sha1(unStr);
+
     if (wx.js_ticket) {
-        ctx.result.setResult({
-            ticket: wx.js_ticket,
-            nonceStr: uuid.v1()
-        })
+        ctx.result.setResult(ret)
     } else {
         requestAccessToken()
     }
